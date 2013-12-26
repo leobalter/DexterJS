887d5407c69db21a582e79050e7d5bef53df7a3a
Leonardo Balter <leonardo.balter@gmail.com>
2013-12-26 04:38:36 -0200

    whitespace

--- test/test-node.js
+++ test/test-node.js
@@ -4,15 +4,14 @@ var Dexter = require( '../src/Dexter' );
 var Dexter_fakeXHR = require( '../src/Dexter.fakeXHR' );
 
 exports.Environment = {
-    'Dexter is Here!': function(test){
-        test.expect(2);
+    'Dexter is Here!' : function ( test ) {
+        test.expect( 2 );
         test.equal( typeof( Dexter ), 'object', 'Dexter is an object');
         test.ok( Dexter, 'Dexter is not falsy' );
         test.done();
     },
-
-    'Dexter functions': function(test){
-        test.expect(3);
+    'Dexter functions' : function ( test ) {
+        test.expect( 3 );
         test.equal( typeof( Dexter.spy ), 'function', 'Dexter.spy is a function' );
         test.equal( typeof( Dexter.fake ), 'function', 'Dexter.fake is a function' );
         test.equal( typeof( Dexter.fakeXHR ), 'undefined', 'Dexter.fakeXHR is a not function on Node environment' );
@@ -21,36 +20,33 @@ exports.Environment = {
 };
 
 exports.Fake = {
-    setUp : function(callback) {
+    setUp : function ( callback ) {
         this.foo = {};
-        this.foo.bar = function(test) {
-            test.expect(1);
+        this.foo.bar = function ( test ) {
+            test.expect( 1 );
             test.ok( false, 'fake shoud not call original method' );
             test.done();
         };
         this.fake = Dexter.fake( this.foo, 'bar' );
         callback();
     },
-
-    'returned object': function(test) {
-        test.expect(2);
-        test.equal( typeof( this.fake ), 'object', 'Dexter.fake returns and object' );
+    'returned object' : function ( test ) {
+        test.expect( 2 );
+        test.equal( typeof this.fake, 'object', 'Dexter.fake returns and object' );
         test.ok( this.fake.isActive, 'fake.isActive === true' );
         test.done();
     },
-
-    'call count': function(test) {
+    'call count' : function ( test ) {
         var i;
-        test.expect(11);
+        test.expect( 11 );
         for ( i = 0 ; i < 11 ; ++i ) {
             test.deepEqual( this.fake.called, i, 'fake.called === ' + i );
             this.foo.bar();
         }
         test.done();
     },
-
-    'restore()': function(test) {
-        this.foo.otherBar = function() {
+    'restore()' : function ( test ) {
+        this.foo.otherBar = function () {
             test.ok( true, 'fake restore objects' );
         };
 
@@ -62,9 +58,8 @@ exports.Fake = {
         test.deepEqual( fake.isActive, false, 'fake.isActive === false after restoring' );
         test.done();
     },
-
-    'callback': function(test) {
-        this.fake.callback = function( a, b, c ) {
+    'callback' : function ( test ) {
+        this.fake.callback = function ( a, b, c ) {
             test.ok( true, '.callback is set' );
             test.deepEqual( [ a, b, c ], [ 1, 2, 3 ], 'callback arguments working' );
         };
@@ -73,7 +68,7 @@ exports.Fake = {
 
         this.fake.restore();
 
-        this.fake = Dexter.fake( this.foo, 'bar', function() {
+        this.fake = Dexter.fake( this.foo, 'bar', function () {
             test.ok( true, 'callback can be set at fake creation' );
             return 17;
         });
@@ -84,30 +79,28 @@ exports.Fake = {
 };
 
 exports.Restore = {
-
-    setUp : function(callback) {
-        this.myMock1 = function() {};
-        this.myMock2 = function() {};
-        this.myMock3 = function() {};
+    setUp : function ( callback ) {
+        this.myMock1 = function () {};
+        this.myMock2 = function () {};
+        this.myMock3 = function () {};
 
         this.fakes = [];
         this.fakes.push( Dexter.fake( this, 'myMock1' ) );
         this.fakes.push( Dexter.fake( this, 'myMock2' ) );
         this.fakes.push( Dexter.spy( this, 'myMock3' ) );
         callback();
-    },
-    tearDown : function(callback) {
+    },    
+    tearDown : function ( callback ) {
         while( this.fakes.length ) {
             this.fakes.pop().restore();
         }
         callback();
     },
-
-    'storing methods': function(test) {
+    'storing methods' : function ( test ) {
         var fakes = this.fakes,
             stored = Dexter.stored,
             item;
-        test.expect(3);
+        test.expect( 3 );
         while ( fakes.length ) {
             item = fakes.pop();
             test.strictEqual( stored.pop(), item, 'Dexter stored item' );
@@ -116,17 +109,15 @@ exports.Restore = {
         }
         test.done();
     },
-
-    'restoring': function (test) {
-        test.expect(3);
+    'restoring' : function ( test ) {
+        test.expect( 3 );
         test.ok( Dexter.stored.length >= 3, 'Dexter.stored has items' );
         test.ok( Dexter.restore(), 'Dexter.restore() => true' );
         test.strictEqual( Dexter.stored.length, 0, 'Dexter.stored empty after Dexter.restore()' );
         test.done();
     },
-
-    'restoring for sure': function (test) {
-        test.expect(1);
+    'restoring for sure' : function ( test ) {
+        test.expect( 1 );
         this.myFn = function () {
             test.ok( true, 'restored function working' );
             test.done();
@@ -144,37 +135,35 @@ exports.Restore = {
 };
 
 exports.Spy = {
-    setUp: function(callback){
+    setUp: function ( callback ) {
         this.foo = {};
-        this.foo.bar = function(test) {
+        this.foo.bar = function ( test ) {
             test.ok( true, 'spy preserve method calling' );
             return 'foo!';
         };
         this.spy = Dexter.spy( this.foo, 'bar' );
         callback();
     },
-
-    'returned object': function(test) {
-        test.expect(5);
+    'returned object' : function ( test ) {
+        test.expect( 5 );
         test.equal( typeof( this.spy ), 'object', 'Dexter.spy returns and object' );
         test.ok( this.spy.isActive, 'spy.isActive === true' );
 
-        test.throws( function() {
+        test.throws( function () {
             Dexter.spy( this.foo, 'whateverDoesntExist' );
         }, 'raises an error if method doesnt exist' );
 
-        test.throws( function() {
+        test.throws( function () {
             Dexter.spy();
         }, 'raises an error without arguments' );
 
-        test.throws( function() {
-            Dexter.spy( this.foo, function() {} );
+        test.throws( function () {
+            Dexter.spy( this.foo, function () {} );
         }, 'raises an error if method name isn\'t a string' );
         test.done();
     },
-
-    'restore()': function(test) {
-        test.expect(3);
+    'restore()' : function ( test ) {
+        test.expect( 3 );
         this.spy.restore();
 
         // 1 ok
@@ -184,9 +173,8 @@ exports.Spy = {
         test.deepEqual( this.spy.isActive, false, 'spy.isActive === false after restoring' );
         test.done();
     },
-
-    'call count': function(test) {
-        test.expect(22);
+    'call count' : function ( test ) {
+        test.expect( 22 );
         var i;
         for ( i = 0 ; i < 11 ; ++i ) {
             test.deepEqual( this.spy.called, i, 'spy.called === ' + i );
@@ -194,10 +182,9 @@ exports.Spy = {
         }
         test.done();
     },
-
-    'arguments': function(test) {
-        test.expect(1);
-        this.foo.otherBar = function( a, b, c ) {
+    'arguments' : function ( test ) {
+        test.expect( 1 );
+        this.foo.otherBar = function ( a, b, c ) {
             test.deepEqual( [ a, b, c ], [ 'Dexter', 'is', 'here!' ], 'keeping arguments in the spied call' );
         };
 
@@ -206,10 +193,9 @@ exports.Spy = {
         this.foo.otherBar( 'Dexter', 'is', 'here!' );
         test.done();
     },
-
-    'callback()': function(test) {
-        test.expect(6);
-        this.spy.callback = function( t, a, b, c ) {
+    'callback()' : function ( test ) {
+        test.expect( 6 );
+        this.spy.callback = function ( t, a, b, c ) {
             test.ok( true, '.callback is set' );
             test.deepEqual( [ t, a, b, c ], [ test, 1, 2, 3 ], 'callback arguments working' );
         };
@@ -218,7 +204,7 @@ exports.Spy = {
 
         this.spy.restore();
 
-        this.spy = Dexter.spy( this.foo, 'bar', function() {
+        this.spy = Dexter.spy( this.foo, 'bar', function () {
             test.ok( true, 'callback can be set at spy creation' );
             return 'bar!';
         });
@@ -226,16 +212,15 @@ exports.Spy = {
         test.strictEqual( this.foo.bar(test), 'foo!', 'spy preserves returned value' );
         test.done();
     },
-
-    'callback() order': function(test) {
-        test.expect(1);
+    'callback() order' : function ( test ) {
+        test.expect( 1 );
         var foo = '';
 
-        Dexter.__bar__ = function() {
+        Dexter.__bar__ = function () {
             foo = 'a';
         };
 
-        Dexter.spy( Dexter, '__bar__', function() {
+        Dexter.spy( Dexter, '__bar__', function () {
             foo += 'b';
         });
 
@@ -244,5 +229,4 @@ exports.Spy = {
         test.strictEqual( foo, 'ab', 'callback called after spied function' );
         test.done();
     }
-
 };
